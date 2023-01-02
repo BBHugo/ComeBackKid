d9c374424788738663246e73e9535fe1bb87b426
Hugo Mendoza <thehugomendoza@gmail.com>
2023-01-01 19:17:31 -0800

    Save 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-document.querySelector('h1').addEventListener('click', getACuteDogPhoto)
+// document.querySelector('h1').addEventListener('click', getACuteDogPhoto)
 
-function getACuteDogPhoto(){
-    const fetchImg = fetch('https://dog.ceo/api/breeds/image/random')
-    const testJson = fetchImg.then(response => response.json())
-    document.querySelector('h2').innerText = Math.ceil(Math.random() * 10)
-    document.querySelector('img').src= testJson
-}
+// function getACuteDogPhoto(){
+//     const fetchImg = fetch('https://dog.ceo/api/breeds/image/random')
+//     const testJson = fetchImg.then(response => response.json())
+//     document.querySelector('h2').innerText = Math.ceil(Math.random() * 10)
+//     document.querySelector('img').src= testJson
+// }
